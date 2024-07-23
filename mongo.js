dfdabfcb1741136b6ab600715eb0be0b236b3bd7
Paing Khant <paingkhant0397@gmail.com>
2024-07-23 11:55:40 +0630

    configured mongodb

--- mongo.js
+++ mongo.js
@@ -1,30 +1,50 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 if (process.argv.length < 3) {
-  console.log('give password a argument')
+  console.log("please give password as argument")
   process.exit(1)
 }
 
-const process = process.argv[2]
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
 
-const url = `mongodb+srv://paingkhant0397:${password}@fullstackopen.xkzz5re.mongodb.net/?retryWrites=true&w=majority&appName=fullStackOpen`
+const url = `mongodb+srv://paingkhant0397:${password}@phonebook.ownkkcm.mongodb.net/phonebook?retryWrites=true&w=majority&appName=phonebook`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
+personSchema = mongoose.Schema({
+  name: String,
+  number: String
 })
 
-const Note = mongoose.model('Note', noteSchema)
+const Person = mongoose.model('Person', personSchema)
 
-const note = new Note({
-  content: 'HTML is easy',
-  important: true
-})
+if (password && !name && !number) {
+  Person.find({}).then(result => {
+    console.log("Person:")
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    });
+    mongoose.connection.close()
+  })
 
-note.save().then((result) => {
-  console.log('note saved!')
-  mongoose.connection.close()
-})
+}
+
+if (name && number) {
+  const person = new Person({
+    name: name,
+    number: number,
+  })
+
+  person.save().then(result => {
+    // console.log("result", result)
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+}
+
+
+
+
